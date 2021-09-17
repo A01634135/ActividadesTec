57f86342930e61b10a8312a6816f3364f33ffdd5
A01634135 <A01634135@itesm.mx>
2021-09-17 03:04:55 -0500

    Implementar convolucion con padding al recibir una imagen y una matriz

--- Act2/padding.py
+++ Act2/padding.py
@@ -1,16 +1,20 @@
+"""
+Programa que implementa y devuelve la convolucion de una imagen con padding, al recibir una imagen y una matriz.
+"""
 
 import numpy as np
 import cv2
 import argparse
 import matplotlib.pyplot as plt
  
-
+ 
+""" Realiza convolucion al recibir una imagen"""
 def convolution(image, kernel, average=False, verbose=False):
 
-    if len(image.shape) == 3:
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    if len(image.shape) == 3: #Si la imagen tiene color
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #Convierte a blanco y negro
   
-    if verbose:
+    if verbose: #Muestra la imagen sin color
         plt.imshow(image, cmap='gray')
         plt.title("Imagen sin color")
         plt.show()
@@ -28,18 +32,18 @@ def convolution(image, kernel, average=False, verbose=False):
  
     padded_image[pad_height:padded_image.shape[0] - pad_height, pad_width:padded_image.shape[1] - pad_width] = image
  
-    if verbose:
+    if verbose: #Muestra la imagen con padding
         plt.imshow(padded_image, cmap='gray')
         plt.title("Imagen con padding")
         plt.show()
  
-    for row in range(image_row):
+    for row in range(image_row): #Implementa la convolucion
         for col in range(image_col):
             output[row, col] = np.sum(kernel * padded_image[row:row + kernel_row, col:col + kernel_col])
             if average:
                 output[row, col] /= kernel.shape[0] * kernel.shape[1]
  
-    if verbose:
+    if verbose: #Muestra la imagen final
         plt.imshow(output, cmap='gray')
         plt.title("Imagen output usando {}X{} Kernel".format(kernel_row, kernel_col))
         plt.show()
@@ -47,11 +51,10 @@ def convolution(image, kernel, average=False, verbose=False):
     return output
 
 
-filtro = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+filtro = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]]) #Sobel Edge (My)
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="Path to the image")
 args = vars(ap.parse_args())
 image = cv2.imread(args["image"])
 
 convolution(image, filtro, verbose = True)
-
